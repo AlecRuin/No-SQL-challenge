f5f63bfb3a612aaa75a033712e62c62664eb6e39
Alec Harvey <ruinhard@gmail.com>
2022-04-30 15:47:18 -0500

    creating thought routes

--- controllers/api/thoughts.js
+++ controllers/api/thoughts.js
@@ -3,21 +3,21 @@ const Thought = require("../../models/Thought")
 
 router.get("/",async(req,res)=>{
     try {
-        var AllThoughts = Thought.find({})
+        var AllThoughts = await Thought.find({})
         res.status(200).json(AllThoughts)
     } catch (error) {
-        console.log(err);
+        console.log(error);
         res.status(500).json(error)
     }
 })
 router.get("/:id",async(req,res)=>{
     try {
-        var SingleThought = Thought.findById(
+        var SingleThought = await Thought.findById(
             {_id:req.params.id}
         )
         res.status(200).json(SingleThought)
     } catch (error) {
-        console.log(err);
+        console.log(error);
         res.status(500).json(error)
     }
 })
